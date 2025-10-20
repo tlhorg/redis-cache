9ceb8b0b527b66ed9ec995f81e303fbf64fe55d6
Linh Pham <pnlinh1207@gmail.com>
2025-10-20 23:15:14 +0700

    add test dependencies and Makefile target for running tests

---
+++ redis_test.go
@@ -0,0 +1,57 @@
+package cache
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRedisCache_Get(t *testing.T) {
+	cache := new(mockRedisClient)
+
+	cache.On("Get", mock.AnythingOfType("string")).Return("value", nil)
+	_, err := cache.Get("key")
+	require.NoError(t, err)
+	cache.AssertExpectations(t)
+}
+
+func TestRedisCache_Set(t *testing.T) {
+	cache := new(mockRedisClient)
+	cache.On(
+		"Set",
+		mock.AnythingOfType("string"),
+		mock.AnythingOfType("string"),
+		mock.AnythingOfType("time.Duration")).Return(nil)
+	err := cache.Set("key", "value", time.Minute*1)
+	require.NoError(t, err)
+	cache.AssertExpectations(t)
+}
+
+func TestRedisCache_Delete(t *testing.T) {
+	cache := new(mockRedisClient)
+	cache.On("Del", mock.AnythingOfType("string")).Return(nil)
+	err := cache.Del("key")
+	require.NoError(t, err)
+	cache.AssertExpectations(t)
+}
+
+type mockRedisClient struct {
+	mock.Mock
+}
+
+func (m *mockRedisClient) Get(key string) (string, error) {
+	args := m.Called(key)
+	return args.String(0), args.Error(1)
+}
+
+func (m *mockRedisClient) Set(key string, value interface{}, expiration time.Duration) error {
+	args := m.Called(key, value, expiration)
+	return args.Error(0)
+}
+
+func (m *mockRedisClient) Del(key string) error {
+	args := m.Called(key)
+	return args.Error(0)
+}
